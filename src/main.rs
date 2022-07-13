3ee2fd13591a12c1f885abee581f0e7f3946d3f8
RekRychiger <109219339+RekRychiger@users.noreply.github.com>
2022-07-13 13:56:32 +0200

    Add timestamp parameter to main.rs
    Timestamp parameter added so that an existing timestamp and user can be passed to the modification updater.
    This way only the signature is recreated, information about the last modification is not lost.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use chrono::Utc;
+use chrono::{DateTime, Utc};
 use modification_updater::{LastModification, ProjectResource};
 use std::env;
 use std::path::Path;
@@ -7,11 +7,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     let path = args.get(1).expect("Must pass a path as first argument");
     let actor = args.get(2).expect("Must pass an actor as second argument");
-
+    let timestamp_string = args.get(3).expect("Must pass a timestamp as third argument");
+	let timestamp : DateTime<Utc> = DateTime::parse_from_rfc3339(timestamp_string).unwrap().with_timezone(&Utc);
+    
     let resource = ProjectResource::from_path(Path::new(path))?;
     let updated = resource.update(LastModification {
         actor: actor.clone(),
-        timestamp: Utc::now(),
+        timestamp: timestamp,
     });
     println!(
         "{}",
